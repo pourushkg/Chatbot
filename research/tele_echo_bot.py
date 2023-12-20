0182dec36ac62f6e77844c9ade91b47137ae545f
Pourush Kumar Gupta <pkg.21p10161@mtech.nitdgp.ac.in>
2023-12-20 18:04:23 +0530

    add hello and same message reply to tele_echo_bot

---
+++ research/tele_echo_bot.py
@@ -0,0 +1,35 @@
+import logging 
+from aiogram import Bot, Dispatcher, executor, types
+from dotenv import load_dotenv 
+import os 
+
+load_dotenv()
+API_TOKEN = os.getenv("TOKEN")
+# print(API_TOKEN)
+
+#configu
+logging.basicConfig(level=logging.INFO)
+
+#Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start','help'])
+async def command_start_handler(message: types.Message):
+    """
+    This handler receives messages with `/start` or '/help' command
+    """
+
+    await message.reply(f"Hi \n I am Echo Bot! \n powered by aiogram.")
+
+@dp.message_handler()
+async def command_start_handler(message: types.Message):
+    """
+    This will return echo 
+    """
+
+    await message.answer(message.text)
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
+
